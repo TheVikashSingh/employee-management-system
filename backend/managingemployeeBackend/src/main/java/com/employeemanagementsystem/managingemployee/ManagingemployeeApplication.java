dee200a6220bcbec7618e8d318190dd54efa3750
= <=>
2025-08-28 18:52:24 +0530

    basic folder creation

---
+++ backend/managingemployeeBackend/src/main/java/com/employeemanagementsystem/managingemployee/ManagingemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsystem.managingemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingemployeeApplication.class, args);
+	}
+
+}
---
+++ backend/managingemployeeBackend/src/test/java/com/employeemanagementsystem/managingemployee/ManagingemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsystem.managingemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
